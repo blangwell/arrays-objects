0296adf14b1d69ecf37d0c4ae3fbf786e28b24c7
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-15 14:03:44 -0700

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = 'Barent';
+console.log(name);
+
+var myLocation = 'Inland Empire';
+console.log(myLocation);
+
+var favoriteNumber = 23;
+console.log(favoriteNumber);
+
+var teams = ['clippers', 'lakers', 'beers', 'rockets', 'nets'];
+console.log(teams);
+
